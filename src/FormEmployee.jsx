bc3c4c3382d35c12c0355f95369bf6e635f181fd
Dina Ramaro <ramaro.dina@gmail.com>
2018-10-23 13:58:47 +0200

    first commit

---
+++ src/FormEmployee.jsx
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import "./App.css"
+
+
+class FormEmployee extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            poster: '',
+            comment: '',
+        }
+        this.onChange = this.onChange.bind(this);
+        this.submitForm = this.submitForm.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    submitForm(e) {
+        e.preventDefault();
+
+        const config = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(this.state),
+           };
+        
+        const url = "http://92.175.11.66:3001/api/quests/movies/";
+
+        fetch(url, config)
+.then(res => res.json())
+ .then(res => {
+   if (res.error) {
+     alert(res.error);
+   } else {
+     alert(`Film ajouté avec l'ID ${res}!`);
+   }
+ }).catch(e => {
+   console.error(e);
+   alert('Erreur lors de l\'ajout d\'un film');
+ });
+
+
+    }
+
+
+    render() {
+
+        return (
+
+            <div className="FormEmployee">
+                <h1>Mon film préféré</h1>
+
+                <form onSubmit={this.submitForm}>
+                    <fieldset>
+                        <legend>Nom du film</legend>
+                        <div className="form-data">
+                            <label htmlFor="name">Titre du Film</label>
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                onChange={this.onChange}
+                                value={this.state.name}
+                            />
+                        </div>
+
+                        <div className="form-data">
+                            <label htmlFor="poster">Url</label>
+                            <input
+                                type="poster"
+                                id="poster"
+                                name="poster"
+                                onChange={this.onChange}
+                                value={this.state.poster}
+                            />
+                        </div>
+
+                        <div className="form-data">
+                            <label htmlFor="comment">Commentaires</label>
+                            <input
+                                type="comment"
+                                id="comment"
+                                name="comment"
+                                onChange={this.onChange}
+                                value={this.state.comment}
+                            />
+                        </div>
+                        <hr />
+                        <div className="form-data">
+                            <input type="submit" value="Envoyer" />
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+
+        )
+
+
+    }
+
+
+
+}
+
+export default FormEmployee;
+
